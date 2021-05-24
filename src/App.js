f9aaf64276cdf453b5a4b77168d7199fc5f23553
abdulghafurjober <abdulghafurjober@hotmail.com>
2021-05-24 22:47:44 +0800

    first initialization

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c9ac704e-3d5c-49bb-809f-de51f9d25e1a/davlolz-f7890045-edcd-48e9-9ac0-04c18e03be7b.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2M5YWM3MDRlLTNkNWMtNDliYi04MDlmLWRlNTFmOWQyNWUxYVwvZGF2bG9sei1mNzg5MDA0NS1lZGNkLTQ4ZTktOWFjMC0wNGMxOGUwM2JlN2IuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.pFgL7OpfdKcwjjA80lfnYjmEvO2Gw5nP6ACQcg_IAkA" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
